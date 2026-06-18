d46d2cffc58d0feffd7d572ce99e0c01a9db4dcd
mihirpatel4175 <mihirpatel4175@gmail.com>
2026-06-18 16:39:57 +0530

    Section Update

---
+++ assets/js/products.js
@@ -0,0 +1,21 @@
+const products = [
+
+{
+    name: "Business Cards",
+    category: "business",
+    image: "https://images.unsplash.com/..."
+},
+
+{
+    name: "Letterheads",
+    category: "business",
+    image: "https://images.unsplash.com/..."
+},
+
+{
+    name: "Brochures",
+    category: "marketing",
+    image: "https://images.unsplash.com/..."
+}
+
+];
